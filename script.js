3f0cb12e1c5becf4298c05ec5ec35e9f1ae9794c
jbk2 <james@humanbenefits.com>
2024-11-13 14:09:13 +0100

    Add new book and update books table

--- script.js
+++ script.js
@@ -4,28 +4,27 @@ document.addEventListener('DOMContentLoaded', () => {
   const addBookBtn = document.getElementById('add-book-btn');
   const booksTable = document.getElementById('books');
 
-
-
-
   function addBook() {
-    let addBookTitle = document.getElementById('add-book-title');
-    let addBookAuthor = document.getElementById('add-book-author');
-    console.log(addBookAuthor.value);
+    const [addBookTitle, addBookAuthor] = document.querySelectorAll('#add-book-title, #add-book-author');
 
+    let row = document.createElement('tr');
+    row.innerHTML = `<td>${addBookTitle.value}</td><td>${addBookAuthor.value}</td>`;
+    booksTable.appendChild(row);
+    
     let newBook = {title: addBookTitle.value , author: addBookAuthor.value}
     books.push(newBook)
+    [addBookTitle, addBookAuthor] = ['', ''];
   }
 
-  addBookBtn.addEventListener('click', addBook);
-
-
   function displayBooks() {
+    // booksTable.innerHTML = "";
     books.forEach((book) => {
       let row = document.createElement('tr');
       row.innerHTML = `<td>${book.title}</td><td>${book.author}</td>`;
       booksTable.appendChild(row);
     })
   };
-
+  
   displayBooks();
+  addBookBtn.addEventListener('click', addBook);
 });
