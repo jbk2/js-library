e3b3f0fc8208b680cd97c52e995036faf9e7bce8
jbk2 <james@humanbenefits.com>
2024-11-13 15:38:37 +0100

    Convert to html form for new book inputs

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   let booksArray = [];
-  const addBookBtn = document.getElementById('add-book-btn');
+  const addBookForm = document.getElementById('add-book-form');
   const booksTable = document.getElementById('books');
 
-  function Book(author, title) {
+  function Book(author, title, noOfPages, read = false) {
     this.author = author;
     this.title = title;
+    this.noOfPages = noOfPages;
+    this.read = read;
   }
 
-  let seedBook1 = new Book('To Kill a Mockingbird', 'Harper Lee');
-  let seedBook2 = new Book('Animal Farm', 'George Orwell');
+  let seedBook1 = new Book('To Kill a Mockingbird', 'Harper Lee', 300, false);
+  let seedBook2 = new Book('Animal Farm', 'George Orwell', 290, true);
   booksArray.push(seedBook1, seedBook2);
 
   function createBook() {
-    let [addBookTitle, addBookAuthor] = document.querySelectorAll('#add-book-title, #add-book-author');
-    let newBook = new Book(addBookTitle.value, addBookAuthor.value);
-    [addBookTitle.value, addBookAuthor.value] = ['', ''];
+    let [
+      addBookTitle,
+      addBookAuthor,
+      addBookPages,
+      addBookRead
+    ] = document.querySelectorAll('#add-book-title, #add-book-author, #add-book-pages, #add-book-read');
+    let newBook = new Book(addBookTitle.value, addBookAuthor.value, addBookPages.value, addBookRead.value);
+    
+    [addBookTitle.value, addBookAuthor.value, addBookPages.value, addBookRead.value] = ['', '', '', ''];
     console.log(newBook);
     return newBook;
   }
@@ -27,21 +35,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function displayBook(book) {
     let row = document.createElement('tr');
-    row.innerHTML = `<td>${book.title}</td><td>${book.author}</td>`;
+    row.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.noOfPages}</td><td>${book.read}</td>`;
     booksTable.appendChild(row);
+    console.log(booksArray);
   };
 
   function displayBooks() {
     booksArray.forEach((book) => {
       let row = document.createElement('tr');
-      row.innerHTML = `<td>${book.title}</td><td>${book.author}</td>`;
+      row.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.noOfPages}</td><td>${book.read}</td>`;
       booksTable.appendChild(row);
     })
+    console.log(booksArray);
   };
   
   displayBooks();
 
-  addBookBtn.addEventListener('click', () => {
+  addBookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
     let newBook = createBook();
     addBookToArray(newBook);
     displayBook(newBook);
